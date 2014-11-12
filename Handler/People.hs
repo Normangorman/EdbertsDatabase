6eb8922ad68ab881acf6fded4b92d04faeb6bd58
Benjamin Gorman <8076bgorman@gmail.com>
2014-11-12 19:03:27 +0000

    add navbar to default layout and make people lookup table a widget

--- Handler/People.hs
+++ Handler/People.hs
@@ -1,18 +1,35 @@
 module Handler.People where
 
 import Import
+-- import Yesod.Request
 
 getPeopleR :: Handler Html
 getPeopleR = do
-    people <- runDB $ selectList 
-        []
-        [Asc PersonLastName, Asc PersonFirstName]
     defaultLayout $ do
+        --this hamlet interpolates the peopleRows widget
         $(widgetFile "people")
 
 fromMaybe :: Show a => Maybe a -> String
 fromMaybe Nothing  = ""
 fromMaybe (Just a) = show a
 
-postPeopleR :: Handler Html
-postPeopleR = error "Not yet implemented: postPeopleR"
+peopleRows :: Widget
+peopleRows = do
+    people <- handlerToWidget $ runDB $ selectList [] [Asc PersonFirstName, Asc PersonLastName]
+    [whamlet|
+        $forall Entity pkey person <- people 
+          <tr>
+            <td><a href=@{PersonR pkey}>#{personFirstName person}
+            <td>#{personLastName person}
+            <td>#{fromMaybe $ personBirthday person}
+            <td>#{fromMaybe $ personHomeNumber person}
+            <td>#{fromMaybe $ personMobileNumber person}
+            <td>#{fromMaybe $ personEmailAddress person}
+            <td>#{fromMaybe $ personGender person}
+    |]
+
+postPeopleR :: Handler ()
+postPeopleR = do
+    query <- runInputPost $ ireq textField "quick_search_query"
+    setMessage (toHtml query)
+    redirect HomeR
