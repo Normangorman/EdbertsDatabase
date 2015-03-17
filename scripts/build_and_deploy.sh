d8e9f1fbd7896a851f682c877d2ce493584fba8d
Benjamin Gorman <8076bgorman@gmail.com>
2015-03-17 23:29:14 +0000

    update build and deploy script

--- scripts/build_and_deploy.sh
+++ scripts/build_and_deploy.sh
@@ -10,4 +10,4 @@ wait
 rsync -rvzh --progress dist/build/EdbertsDatabase/EdbertsDatabase root@178.62.94.90:/srv/EdbertsDatabase
 wait
 
-ssh root@gormangames.com "start edberts"
+ssh root@178.62.94.90 "start edberts"
